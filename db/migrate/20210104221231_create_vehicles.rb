6c5fa012d7ad276eaf5d2c26efa23546bca9b76b
asb1512 <andrew@olympicmedia.com>
2021-01-04 17:18:48 -0500

    Updated Vehicle migration file.

--- db/migrate/20210104221231_create_vehicles.rb
+++ db/migrate/20210104221231_create_vehicles.rb
@@ -1,8 +1,12 @@
 class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
-
-      t.timestamps null: false
+      t.integer :model_year
+      t.string :make
+      t.string :model
+      t.integer :mileage
+      t.integer :user_id
+      t.timestamps
     end
   end
 end
