21a9de3d9ec137bd96bf6ce0e378ad3b980d87c1
asb1512 <andrew@olympicmedia.com>
2021-01-04 17:22:02 -0500

    Updated Entry migration file.

--- db/migrate/20210104221331_create_entries.rb
+++ db/migrate/20210104221331_create_entries.rb
@@ -1,8 +1,11 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-
-      t.timestamps null: false
+      t.string :entry_type
+      t.string :entry_content
+      t.date :due_date
+      t.integer :vehicle_id
+      t.timestamps
     end
   end
 end
