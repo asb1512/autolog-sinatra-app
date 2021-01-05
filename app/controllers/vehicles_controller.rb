729b653d1850c5729ebd2ea0e900874016567626
asb1512 <andrew@olympicmedia.com>
2021-01-05 13:13:33 -0500

    Added further funcitonality to /vehicles/new form and user account page.

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,9 +1,13 @@
 class VehiclesController < ApplicationController
    get '/vehicles/new' do
-      erb :'/vehicles/new'
+      if logged_in?
+         erb :'/vehicles/new'
+      else
+         flash[:message] = "<p>You are not logged in. Please login or create an account.</p><p><a href='/signup'>Sign Up</a></p><p><a href='/login'>Login</a></p>"
+      end
    end
 
    post '/vehicles' do
-      
+
    end
 end
