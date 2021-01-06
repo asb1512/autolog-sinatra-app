cc07c6b285480dae2e976a843599d45b28f3cd33
asb1512 <andrew@olympicmedia.com>
2021-01-05 22:48:15 -0500

    Added radio buttons to /entries/new form to associate an entry with a vehicle.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,4 +8,12 @@ class EntriesController < ApplicationController
       end
    end
 
+   post '/entries' do
+      if params[:due_date]
+         entry = Entry.create(entry_type: params[:entry_type], entry_content: params[:entry_content], due_date: params[:due_date])
+      else
+         entry = Entry.create(entry_type: params[:entry_type], entry_content: params[:entry_content])
+      end
+   end
+
 end
