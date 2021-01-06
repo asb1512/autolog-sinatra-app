3a74507369e3bab1a022c1f10e8911b46f3ae349
asb1512 <andrew@olympicmedia.com>
2021-01-05 22:38:54 -0500

    Updated /entries/new form.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,11 @@
 class EntriesController < ApplicationController
 
    get '/entries/new' do
-      erb :'/entries/new'
+      if logged_in?
+         erb :'/entries/new'
+      else
+         flash[:message] = "<p>You are not logged in. Please login or sign up.</p><p>Back to <a href='/login'>Login</a> or <a href='/signup'>Sign Up</a>.</p>"
+      end
    end
 
 end
