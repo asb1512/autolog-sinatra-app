04bff3f80481f32a26ccd900640450e702f76512
asb1512 <andrew@olympicmedia.com>
2021-01-06 00:40:50 -0500

    Updated POST route /entries to associate Entry with its Vehicle.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -9,12 +9,19 @@ class EntriesController < ApplicationController
    end
 
    post '/entries' do
-      if params[:entry_type].empty? || params[:entry_content].empty?
-         flash[:message] = "<p>You must include a maintenance type and a description. <a href='/entries/new'>Return to Entry</a></p>"
+      binding.pry
+      if params[:entry_type].empty? || params[:entry_content].empty? || params[:vehicles]
+         flash[:message] = "<p>You must include a maintenance type, a description, and an assigned vehicle. <a href='/entries/new'>Return to Entry</a></p>"
       elsif params[:due_date]
          entry = Entry.create(entry_type: params[:entry_type], entry_content: params[:entry_content], due_date: params[:due_date])
+         vehicle = Vehicle.find(params[:vehicles].first)
+         vehicle << entry
+         vehicle.save
       else
          entry = Entry.create(entry_type: params[:entry_type], entry_content: params[:entry_content])
+         vehicle = Vehicle.find(params[:vehicles].first)
+         vehicle << entry
+         vehicle.save
       end
    end
 
