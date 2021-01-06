54392e8196d69363b744ad3b72bd9874dd733608
asb1512 <andrew@olympicmedia.com>
2021-01-06 01:14:40 -0500

    Creation of #date_of_creation to correctly format create_at date for a User's entry.

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,10 @@
 class Entry < ActiveRecord::Base
    belongs_to :vehicle
+
+   def date_of_creation
+      date_str = self.created_at.to_s
+      date_array = date_str.split(" ")
+      isolated_array = date_array[0].split("-")
+      "Created #{isolated_array[1]}/#{isolated_array[2]}/#{isolated_array[0]}"
+   end
 end
