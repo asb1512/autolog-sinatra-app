e861c0ab4c8c1853d38c83f51b11e2e2525fd668
asb1512 <andrew@olympicmedia.com>
2021-01-08 15:49:22 -0500

    Added email verification to form in /users/edit form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,23 @@ class UsersController < ApplicationController
 
    post '/signup' do
       if params[:password] != params[:password_confirmation]
-      flash[:message] = "<p>Your passwords do not match. Please try again.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
+         flash[:message] = "<p>Your passwords do not match. Please try again.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
       elsif User.find_by(email: params[:email])
-      flash[:message] = "<p>This email is already taken. Please use a different email address.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
+         flash[:message] = "<p>This email is already taken. Please use a different email address.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
       elsif !!/\w{1}@\w{1}/.match(params[:email]) && !!/\S{7}\W{1}/.match(params[:password])
-      user = User.create(email: params[:email], password: params[:password])
-      session[:user_id] = user.id
-      flash[:message] = "<p>Your account was successfully created.</p><p>Back to <a href='/users/#{current_user.email}'>Profile</a></p>"
+         user = User.create(email: params[:email], password: params[:password])
+         session[:user_id] = user.id
+         flash[:message] = "<p>Your account was successfully created.</p><p>Back to <a href='/users/#{current_user.email}'>Profile</a></p>"
       else
-      flash[:message] = "<p>Your email or password is invalid. Please try again.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
+         flash[:message] = "<p>Your email or password is invalid. Please try again.</p><p>Back to <a href='/signup'>Sign Up</a></p>"
       end
    end
 
    get '/login' do
       if !logged_in?
-      erb :'users/login'
+         erb :'users/login'
       else
-      redirect "/#{current_user.email}"
+         redirect "/#{current_user.email}"
       end
    end
 
@@ -30,12 +30,12 @@ class UsersController < ApplicationController
       @user = User.find_by(email: params[:email])
       
       if @user && @user.authenticate(params[:password])
-      session
-      session[:user_id] = @user.id
+         session
+         session[:user_id] = @user.id
       
-      redirect to "/#{@user.email}"
+         redirect to "/#{@user.email}"
       else
-      flash[:message] = "<p>Your email or password is invalid. Please try again.</p><p>Back to <a href='/login'>Login</a></p>"
+         flash[:message] = "<p>Your email or password is invalid. Please try again.</p><p>Back to <a href='/login'>Login</a></p>"
       end
    end
 
@@ -65,8 +65,9 @@ class UsersController < ApplicationController
    end
 
    patch '/:email' do
-      if logged_in?
+      if logged_in? && !!/\w{1}@\w{1}/.match(params[:email])
          current_user.update(email: params[:email])
+         flash[:message] = "<p>Your email was successfully updated.</p><p>Back to <a href='/users/#{current_user.email}'>Profile</a></p>"
       else
          flash[:message] = "<p>You are not logged in. Please login or sign up.</p><p>Back to <a href='/login'>Login</a> or <a href='/signup'>Sign Up</a>.</p>"
       end
